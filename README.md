063c242523cf03d7a0a567a0bfdd67af3e20525a
thomas <thomas.ries-balsamo@epitech.eu>
2022-01-19 08:16:57 +0100

    feat(sw/readme) : Ajout de la version du SW et listener / maj du readme sur life cycle d'un SW

--- README.md
+++ README.md
@@ -7,3 +7,20 @@
 ## Google Doc PWA
 
 <https://developers.google.com/web/ilt/pwa?hl=en>
+
+## Cycles de vie
+
+<https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle?hl=en>
+
+Lorsque l'on charge la page pour la toute première fois, le service worker va lancer un processus d'installation.
+
+Une fois installé il va devenir actif mais ne pas fonctionner sur la page courante, ne pas la controller. Si on réactualise la page il va controller les choses et du coup, et le fetch va agir et nous pourrons intercepter les choses.
+
+A chaque fois que je charge la page en étant connecté, le navigateur va essayer de charger la nouvelle version de sw.js.
+
+* Si pas de changements, il ne fais rien
+* Si changement, il va déclencher une nouvelle installation (cf cont PREFIX dans ./sw.js)
+
+=> Si nouvelle installation, il ne s'activera que lorsque le précédent aura fini de fonctionner. C'est à dire, pour le précédent, quand on aura par exemple fermé tous les onglets ou changé de page pendant plusieurs secondes. Quand on va recharger la page, le nouveau va s'installer -> voir const PREFIX dans /sw.js
+
+=> Si on veut skipper cet attente et remplacer l'ancien sw de suite -> voir /sw.js dans l'eventListener install
--- sw.js
+++ sw.js
@@ -3,12 +3,31 @@
 // Il est intégré au index.html au sein d'une balise script chargeant le navigator
 // C'est un Worker séparé, le js n'est pas chargé dans la page donc pas d'accès aux objets du DOM 
 
+// Const qui sert à définir la version du sw pour réinstallation par le navigateur si nouveautés. cf Readme section Cycles de vie)
+const PREFIX = 'V2';
+// const PREFIX = 'V1';
+
+// Le système n'écoute que le second chargement de la page
+self.addEventListener('install', () => {
+    //SlipWaiting permet de ne pas attendre pour charger une nouvelle version du sw
+    self.skipWaiting();
+    console.log(`${PREFIX} Install`);
+});
+
+self.addEventListener('activate', () => {
+    // clients.claim() permet de controller automatiquement la page quand le SW s'active.
+    clients.claim();
+    console.log(`${PREFIX} Active`);
+})
+
 
 // Traitement du offline requiert un handler sur "fetch"
 // Fetch se compare à un proxy qui permet de rajouer des comportements particuliers quand on essaye d'accéder à une ressource -> si ressource pas dispo alors on fait quelque chose de particulier
 self.addEventListener('fetch', (event) => {
     // Permet de récupérer la requête demandée actuellement : l'url et le mode
-    console.log(`Fetching : ${event.request.url}, Mode : ${event.request.mode}`)
+    console.log(
+        `${PREFIX} Fetching : ${event.request.url}, Mode : ${event.request.mode}`
+    );
 
     if(event.request.mode === 'navigate') {
         // respondWith: attend une promesse et pas une function qui renvoie une promesse. On l'appelle de suite avec ()
